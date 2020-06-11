598fad84d3bfeec7cc36dfdf1efc1e6106c88237
methylDragon <methylDragon@gmail.com>
2020-06-11 14:50:13 +0800

    Implement rmf_fleet_adapter bindings

---
+++ rmf_fleet_adapter_python/src/adapter.cpp
@@ -0,0 +1,119 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/iostream.h>
+#include <pybind11/chrono.h>
+#include <pybind11/eigen.h>
+#include <pybind11/stl.h>
+
+#include <memory>
+
+#include "rmf_fleet_adapter/agv/Adapter.hpp"
+#include "rmf_fleet_adapter_python/PyRobotCommandHandle.hpp"
+
+namespace py = pybind11;
+namespace agv = rmf_fleet_adapter::agv;
+
+void bind_types(py::module &);
+void bind_graph(py::module &);
+void bind_shapes(py::module &);
+void bind_vehicletraits(py::module &);
+void bind_plan(py::module &);
+// void bind_tests(py::module &);
+
+PYBIND11_MODULE(rmf_adapter, m) {
+    bind_types(m);
+    bind_graph(m);
+    bind_shapes(m);
+    bind_vehicletraits(m);
+    bind_plan(m);
+    // bind_tests(m);
+
+    // ROBOTCOMMAND HANDLE =====================================================
+    // Abstract class
+    py::class_<agv::RobotCommandHandle, PyRobotCommandHandle,
+               std::shared_ptr<agv::RobotCommandHandle> > \
+      (m, "RobotCommandHandle")  // , py::dynamic_attr())
+        .def(py::init<>())
+        .def("follow_new_path", &agv::RobotCommandHandle::follow_new_path)
+        .def("stop", &agv::RobotCommandHandle::stop)
+        .def("dock", &agv::RobotCommandHandle::dock);
+
+    // ROBOTUPDATE HANDLE ======================================================
+    py::class_<agv::RobotUpdateHandle> \
+      (m, "RobotUpdateHandle")
+        // Private constructor: Only to be constructed via FleetUpdateHandle!
+        .def("interrupted", &agv::RobotUpdateHandle::interrupted)
+        .def("update_position",
+             py::overload_cast<std::size_t, double> \
+               (&agv::RobotUpdateHandle::update_position),
+             py::arg("waypoint"),
+             py::arg("orientation"),
+             py::call_guard<py::scoped_ostream_redirect,
+                            py::scoped_estream_redirect>())
+        .def("update_position",
+             py::overload_cast<const Eigen::Vector3d&,
+                               const std::vector<std::size_t>& > \
+               (&agv::RobotUpdateHandle::update_position),
+             py::arg("position"),
+             py::arg("lanes"),
+             py::call_guard<py::scoped_ostream_redirect,
+                            py::scoped_estream_redirect>())
+        .def("update_position",
+             py::overload_cast<const std::string&,
+                               const Eigen::Vector3d&,
+                               const double,
+                               const double,
+                               const double> \
+               (&agv::RobotUpdateHandle::update_position),
+             py::arg("map_name"),
+             py::arg("position"),
+             py::arg("max_merge_waypoint_distance") = 0.1,
+             py::arg("max_merge_lane_distance") = 1.0,
+             py::arg("min_lane_length") = 1e-8,
+             py::call_guard<py::scoped_ostream_redirect,
+                            py::scoped_estream_redirect>());
+
+    // FLEETUPDATE HANDLE ======================================================
+    py::class_<agv::FleetUpdateHandle,
+               std::shared_ptr<agv::FleetUpdateHandle> > \
+                  (m, "FleetUpdateHandle")
+        // NOTE(CH3): Might have to publicise this constructor if required
+        // Otherwise, it's constructable via agv::TestScenario
+        // .def(py::init<>())  // Private constructor!
+
+        // Also, intentionally missing accept_delivery_requests
+        .def("add_robot", &agv::FleetUpdateHandle::add_robot,
+             // Saved for posterity...
+             /* py::overload_cast<std::shared_ptr<agv::RobotCommandHandle>,
+                               const std::string&,
+                               const rmf_traffic::Profile&,
+                               rmf_traffic::agv::Plan::StartSet,
+                               std::function<void(
+                                 std::shared_ptr<RobotUpdateHandle> handle)>
+                                 > \
+               (&agv::FleetUpdateHandle::add_robot), */
+             py::arg("command"),
+             py::arg("name"),
+             py::arg("profile"),
+             py::arg("start"),
+             py::arg("handle_cb"));
+
+    // ADAPTER =================================================================
+    // Light wrapper
+    py::class_<rclcpp::NodeOptions>(m, "NodeOptions");
+
+    py::class_<agv::Adapter, std::shared_ptr<agv::Adapter> >(m, "Adapter")
+        // .def(py::init<>())  // Private constructor
+        .def_static("make", &agv::Adapter::make,
+             py::arg("node_name"),
+             py::arg("node_options") = rclcpp::NodeOptions(),
+             py::arg("wait_time") = std::chrono::minutes(1))
+        .def("add_fleet", &agv::Adapter::add_fleet,
+             py::arg("fleet_name"),
+             py::arg("traits"),
+             py::arg("navigation_graph"))
+        // .def_property_readonly("Node")  // Needless for Python API currently
+        // .def_property_readonly("ConstNode")
+        .def("start", &agv::Adapter::start)
+        .def("stop", &agv::Adapter::stop);
+}
